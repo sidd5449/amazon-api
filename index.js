c16a09fa9d054688c7d5edb706383666e72e40fc
sidd5449 <76845396+sidd5449@users.noreply.github.com>
2022-03-22 23:30:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const request = require('request-promise')
+
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+const apiKey = '4440b8699e69623753fdb10b6f3998e0';
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
+app.use(express.json());
+
+
+app.get('/', (req, res) =>{
+    res.send('Server started....');
+});
+
+// Product details
+app.get('/products/:productId', async (req, res) =>{
+    const { productId } = req.params;
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/dp/${productId}`);
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+});
+//  Product Reviews
+app.get('/products/:productId/reviews', async (req, res) =>{
+    const { productId } = req.params;
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`);
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+});
+//  Product Offers
+app.get('/products/:productId/offers', async (req, res) =>{
+    const { productId } = req.params;
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+});
+//  Product Search
+app.get('/search/:searchQuery', async (req, res) =>{
+    const { searchQuery } = req.params;
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/s?k=${searchQuery}`);
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+});
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
